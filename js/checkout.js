3f8528e9a636ffc87f3434f082621e4d7ed72638
Gift Mahlangu <giftnkz6@gmail.com>
2022-12-10 21:47:56 +0200

    Added styles

--- js/checkout.js
+++ js/checkout.js
@@ -8,7 +8,7 @@ function showCheckout () {
         
                 <td><img src= "${item.image}" class="card-img-top" alt="..."></td>
                 <td>${item.name}</td>
-                <td>${item.name}</td>
+                <td>${item.colour}</td>
                 <td>R ${item.price}</td>
     
             </div>
@@ -22,10 +22,11 @@ function showCheckout () {
 showCheckout()
 
 function checkoutTotal() {
-    let checkoutPrice = JSON.parse(localStorage.getItem('checkoutStore'));
-    console.log(checkoutPrice);
-    let totalPrice = checkoutPrice.reduce((sum1, sum2) => {
-        return sum1.price + sum2.price
+    let checkOutPrice = JSON.parse(localStorage.getItem('checkoutStore'));
+    // console.log(price);
+    const totalPrice = checkOutPrice.reduce((sum1, sum2) => {
+        return (sum1.price + sum2.price)
+        
     });
 
     document.getElementById("totalCalculation").innerHTML += `
